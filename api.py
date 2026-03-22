e8115cc98d132026023245f3bc98e866c8769b3a
andres05tmm <andresfmalo05@gmail.com>
2026-03-22 17:29:39 -0500

    Update api.py

--- api.py
+++ api.py
@@ -20,7 +20,7 @@ import openpyxl
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
 from typing import Optional, Union
 
